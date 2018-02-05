d5cb37689c3cdf21b822787f973e3c490a8aee4a
Chee Seng Leong <macintoshleong@gmail.com>
2018-02-05 23:49:01 +0800

    Added shepherd library

---
+++ learn.js
@@ -0,0 +1,63 @@
+var tour;
+
+tour = new Shepherd.Tour({
+    defaults: {
+        classes: 'shephred-element shepherd-theme-arrows',
+        scrollTo: true,
+        showCancelLink: true
+    }
+});
+
+tour.addStep('example-step', {
+    title: 'Testing',
+    text: 'Appear Pls <code>HTML 5 rocks</code>',
+    attachTo: '.small-navbar-brand bottom',
+    buttons: [
+        {
+            text: 'Next',
+            action: tour.next
+        }
+    ]
+});
+
+tour.addStep('test', {
+    title: 'Testing 2',
+    text: 'Hello World',
+    attachTo: '.small-navbar-links bottom',
+    classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
+    buttons: [
+        {
+            text: 'Back',
+            classes: 'shepherd-button-secondary',
+            action: tour.back
+        }, {
+            text: 'Next',
+            classes: 'shepherd-button-example-primary',
+            action: tour.next
+        }
+    ]
+});
+
+tour.addStep('test-2', {
+    title: 'Testing 3',
+    text: 'Hello World',
+    attachTo: '.content h3 bottom',
+    classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
+    when: {
+        show: function() {
+            $("content").removeClass("content");
+        }
+    },
+    buttons: [
+        {
+            text: 'Back',
+            classes: 'shepherd-button-secondary',
+            action: tour.back
+        }, {
+            text: 'Next',
+            classes: 'shepherd-button-example-primary',
+            action: tour.next
+        }
+    ]
+});
+tour.start();
