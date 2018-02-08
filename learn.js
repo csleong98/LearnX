2bd64fd01490042c9fd8a0336d4dde0bc3358c54
Chee Seng Leong <macintoshleong@gmail.com>
2018-02-08 11:48:20 +0800

    tutorial model testing

--- learn.js
+++ learn.js
@@ -13,6 +13,12 @@ tour.addStep('example-step', {
     text: 'Appear Pls <code>HTML 5 rocks</code>',
     attachTo: '.small-navbar-brand bottom',
     buttons: [
+
+        {
+            text: 'Exit',
+            classes: 'shepherd-button-secondary',
+            action: tour.hide
+        },
         {
             text: 'Next',
             action: tour.next
@@ -52,6 +58,24 @@ tour.addStep('test-2', {
             text: 'Next',
             classes: 'shepherd-button-example-primary',
             action: tour.next
+        }
+    ]
+});
+
+tour.addStep('test-4', {
+    title: 'Testing 4',
+    text: 'Hello World',
+    attachTo: '.fully-responsive p bottom',
+    classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
+    buttons: [
+        {
+            text: 'Back',
+            classes: 'shepherd-button-secondary',
+            action: tour.back
+        }, {
+            text: 'Exit',
+            classes: 'shepherd-button-example-primary',
+            action: tour.hide
         }
     ]
 });
