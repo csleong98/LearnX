aa1f681b65d1e20584f18d80ca5ee84cae2fdac8
Chee Seng Leong <macintoshleong@gmail.com>
2018-03-01 23:09:43 +0800

    php updates

--- php/register.php
+++ php/register.php
@@ -1,5 +1,14 @@
 <?php 
 
-    
+    if(isset($_POST['fullname']) && !empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['confirm-password'])){
+        $fullname = mysqli_escape_string($_POST['fullname']);
+        $username = mysqli_escape_string($_POST['username']);
+        $password = mysqli_escape_string($_POST['password']);
+        $confirm_password = mysqli_escape_string($_POST['confirm-password']);
+     
+        if(!eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email)) {
+            
+        }
+    }
 
 ?>
