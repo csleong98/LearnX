be323f623e86ee069a954ae63ac3ad7f90efedb8
LEONG CHEE SENG <TP042237@mail.apu.edu.my>
2018-03-02 10:15:27 +0800

    php updates

--- php/register.php
+++ php/register.php
@@ -1,14 +1,25 @@
 <?php 
 
-    if(isset($_POST['fullname']) && !empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['confirm-password'])){
-        $fullname = mysqli_escape_string($_POST['fullname']);
-        $username = mysqli_escape_string($_POST['username']);
-        $password = mysqli_escape_string($_POST['password']);
-        $confirm_password = mysqli_escape_string($_POST['confirm-password']);
-     
-        if(!eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email)) {
-            
-        }
+    $conn = mysqli_connect("boleh.tv", "boleh", "TWSB2018", "boleh_LearnX");
+
+    if(mysqli_connect_errno()) {
+        echo '<script>';
+        echo 'alert("No Connection")';
+        echo '</script>';
     }
+
+    else {
+        echo '<script>';
+        echo 'alert("Connected")';
+        echo '</script>';    }
+
+    $username = mysqli_escape_string($_POST['username']);
+    $fullname = mysqli_escape_string($_POST['fullname']);
+    $email = mysqli_escape_string($_POST['email']);
+    $password = mysqli_escape_string($_POST['password']);
+    $confirm_password = mysqli_escape_string($_POST['confirm-password']);
+    
+    
+    
 
 ?>
