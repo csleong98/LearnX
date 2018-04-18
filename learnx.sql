9055aa7002b83cf96c9f9e997fc751a2960f1a31
Chee Seng Leong <macintoshleong@gmail.com>
2018-04-19 00:28:17 +0800

    added new tables in db

--- learnx.sql
+++ learnx.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 06, 2018 at 01:57 AM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 18, 2018 at 04:26 PM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,45 +25,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tutorial`
+--
+
+DROP TABLE IF EXISTS `tutorial`;
+CREATE TABLE IF NOT EXISTS `tutorial` (
+  `stepID` int(100) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `step1` varchar(100) NOT NULL DEFAULT '0',
+  `step2` varchar(100) NOT NULL DEFAULT '0',
+  `step3` varchar(100) NOT NULL DEFAULT '0',
+  `step4` varchar(100) DEFAULT '0',
+  PRIMARY KEY (`stepID`)
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tutorial`
+--
+
+INSERT INTO `tutorial` (`stepID`, `username`, `step1`, `step2`, `step3`, `step4`) VALUES
+(12, 'cwleong99', '1', '0', '0', '0');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(100) NOT NULL,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL,
   `realname` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `confirm_password` varchar(100) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `confirm_password` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `realname`, `email`, `password`, `confirm_password`) VALUES
-(1, '%^&*()', 'kar seng', 'han980218145381@gmail.com', '123', '123'),
-(2, 'csleong', 'Seng', 'macintoshleong@gmail.com', '123', '123');
+(12, 'cwleong99', 'Chee Weng', 'ms.catwong@gmail.com', '123', '123');
+COMMIT;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
